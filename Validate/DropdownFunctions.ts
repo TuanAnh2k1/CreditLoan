265613a688e8bc46cf5ef40094a77cf33f5d1611
TuanAnh2k1 <tuananh2001ptit@gmail.com>
2023-07-26 10:03:43 +0700

    anhdt/3I5D8gTl-unit-test-info-other (#8)
    Co-authored-by: ThanhMumeSoft <thanhlx@mume>

--- Validate/DropdownFunctions.ts
+++ Validate/DropdownFunctions.ts
@@ -17,3 +17,19 @@ export function validItemsProduct(
   const validItemsProducts = jsonData.map(item => item.value);
   return validItemsProducts.includes(itemsProduct);
 }
+
+export function validBranch(
+  itemsProduct: string,
+  jsonData: Array<any>,
+): boolean {
+  const validBranchs = jsonData.map(item => item.value);
+  return validBranchs.includes(itemsProduct);
+}
+
+export function validChannel(
+  itemsProduct: string,
+  jsonData: Array<any>,
+): boolean {
+  const validChannels = jsonData.map(item => item.value);
+  return validChannels.includes(itemsProduct);
+}
